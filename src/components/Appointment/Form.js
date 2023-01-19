1246c80719135b6d319399e9f4763a5324851e7e
sandeepkaurs <sandeep.singh@telus.com>
2023-01-18 16:01:33 -0800

    Commit changes up to our test firsts

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,40 +4,61 @@ import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  const [name, setName] = useState(props.name|| "");
-  
-  const reset = function(){
+  const [name, setName] = useState(props.name || "");
+  const [error, setError] = useState("");
+
+  const reset = function () {
     setName("");
     setInterviewer(null);
   }
 
-  const cancel = function(){
+  const cancel = function () {
     reset();
     props.onCancel();
   }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    props.onSave(name, interviewer.id);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={props.name}
+            name="name" //{props.name}
             type="text"
             placeholder="Enter Student Name"
             value={name}
-            onChange={(event) => {setName(event.target.value)}}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
-        interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer}
+          interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={event => validate()}>Save</Button>
         </section>
       </section>
     </main>
   )
 }
+
+// <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
-
+import PropTypes from 'prop-types';
 
 
 export default function InterviewerList(props) {
@@ -26,3 +26,6 @@ export default function InterviewerList(props) {
   )
 }
 
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
